8b45557fd326527aad15f94b8152b081eb66c89b
tandat198 <tandat198@gmail.com>
2020-06-13 09:50:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,30 +4,24 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Table from "react-bootstrap/Table";
+import Alert from "react-bootstrap/Alert";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
-
-let tasks = [];
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showTaskList: true,
-            showModal: false,
+            showForm: false,
             taskName: "",
-            taskStatus: 0,
+            taskStatus: -1,
             search: "",
+            tasks: [],
         };
     }
 
-    toggleTaskList = () => {
-        this.setState({ showTaskList: !this.state.showTaskList });
-    };
-
-    toggleTaskModal = () => {
-        this.setState({ showModal: !this.state.showModal });
+    toggleForm = () => {
+        this.setState({ showForm: !this.state.showForm });
     };
 
     handleTaskName = (e) => {
@@ -38,105 +32,117 @@ class App extends Component {
         this.setState({ taskStatus: e.target.value });
     };
 
-    submitForm = () => {
+    handleSearch = (e) => {
+        this.setState({ search: e.target.value });
+    };
+
+    submitForm = (e) => {
+        e.preventDefault();
         const { taskName, taskStatus } = this.state;
-        let statusName;
+        const newTask = {
+            id: uuidv4(),
+            name: taskName,
+        };
         switch (parseInt(taskStatus)) {
             case 0:
-                statusName = "Not Started";
+                newTask.status = "Not Started";
                 break;
             case 1:
-                statusName = "In Progress";
+                newTask.status = "In Progress";
                 break;
             case 2:
-                statusName = "Finished";
+                newTask.status = "Finished";
                 break;
             default:
                 break;
         }
-        tasks.push({ name: taskName, status: statusName });
-        this.setState({ showModal: false });
+        this.setState({
+            tasks: [...this.state.tasks, newTask],
+            taskStatus: -1,
+            taskName: "",
+        });
     };
 
-    handleSearch = (e) => {
-        this.setState({ search: e.target.value });
+    clearAll = () => {
+        this.setState({ tasks: [] });
+    };
+
+    resetInput = () => {
+        this.setState({
+            taskName: "",
+            taskStatus: -1,
+        });
+    };
+
+    deleteTask = (id) => {
+        this.setState({ tasks: this.state.tasks.filter((task) => task.id !== id) });
     };
 
     render() {
         return (
             <div>
-                <Container fluid>
-                    <Form className='mt-3'>
-                        <Row>
-                            <Col xs={12} lg={8}>
+                <Container>
+                    <Row className='mt-3'>
+                        <Col xs={12} sm={8} lg={10}>
+                            <Form onSubmit={this.submitSearchForm}>
                                 <Form.Group className='d-b' controlId='formBasic'>
                                     <Form.Control onChange={this.handleSearch} type='text' placeholder='Search task' />
                                 </Form.Group>
-                            </Col>
-                            <Col xs={6} lg={2}>
-                                <Button onClick={this.toggleTaskList} className='w-100' variant='outline-secondary' type='button'>
-                                    {this.state.showTaskList ? "Hide Task List" : "Show Task List"}
-                                </Button>
-                            </Col>
-                            <Col xs={6} lg={2}>
-                                <Button onClick={this.toggleTaskModal} className='w-100' variant='primary' type='button'>
-                                    Add Task
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                    <Table className='mt-3' striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Task Name</th>
-                                <th>Task Status</th>
-                            </tr>
-                        </thead>
-                        {this.state.showTaskList && (
-                            <tbody>
-                                {tasks
-                                    .filter((task) => task.name.includes(this.state.search))
-                                    .map((task, index) => (
-                                        <tr key={index}>
-                                            <td>{index + 1}</td>
-                                            <td>{task.name}</td>
-                                            <td>{task.status}</td>
-                                        </tr>
-                                    ))}
-                            </tbody>
-                        )}
-                    </Table>
-
-                    <Modal show={this.state.showModal} onHide={this.toggleTaskModal}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Add New Task</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <Form>
-                                <Form.Group controlId='formBasicEmail'>
-                                    <Form.Label>Task Name</Form.Label>
-                                    <Form.Control onChange={this.handleTaskName} type='email' placeholder='Enter task name' />
-                                </Form.Group>
-                                <Form.Group controlId='exampleForm.ControlSelect1'>
-                                    <Form.Label>Status</Form.Label>
-                                    <Form.Control onChange={this.handleStatus} as='select'>
-                                        <option value={0}>Not Started</option>
-                                        <option value={1}>In Progress</option>
-                                        <option value={2}>Finished</option>
-                                    </Form.Control>
-                                </Form.Group>
                             </Form>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button onClick={this.toggleTaskModal} variant='secondary'>
-                                Close
+                        </Col>
+                        <Col xs={12} sm={4} lg={2}>
+                            <Button onClick={this.toggleForm} className='w-100' variant='primary' type='button'>
+                                Show Form
                             </Button>
-                            <Button onClick={this.submitForm} variant='primary'>
-                                Save
-                            </Button>
-                        </Modal.Footer>
-                    </Modal>
+                        </Col>
+                    </Row>
+                    {this.state.showForm && (
+                        <Form className='mt-2' onSubmit={this.submitForm}>
+                            <Row>
+                                <Col xs={8} lg={5} xl={6}>
+                                    <Form.Group className='d-b'>
+                                        <Form.Control value={this.state.taskName} onChange={this.handleTaskName} type='text' placeholder='Enter task name' />
+                                    </Form.Group>
+                                </Col>
+                                <Col xs={4} lg={2}>
+                                    <Form.Group controlId='exampleForm.ControlSelect1'>
+                                        <Form.Control value={this.state.taskStatus} onChange={this.handleStatus} as='select'>
+                                            <option value={-1}>Status</option>
+                                            <option value={0}>Not Started</option>
+                                            <option value={1}>In Progress</option>
+                                            <option value={2}>Finished</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Col>
+                                <Col xs={12} lg={5} xl={4}>
+                                    <Button type='submit' className='mr-2' variant='outline-primary'>
+                                        Add Task
+                                    </Button>
+                                    <Button onClick={this.resetInput} type='reset' className='mx-2' variant='outline-danger'>
+                                        Reset
+                                    </Button>
+                                    <Button onClick={this.clearAll} type='button' className='ml-2' variant='danger'>
+                                        Clear All Tasks
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    )}
+                    {this.state.tasks
+                        .filter((task) => task.name.includes(this.state.search))
+                        .map((task, i) => (
+                            <Alert key={i} className='clearfix' variant={task.status === "Finished" ? "success" : "secondary"}>
+                                <Row>
+                                    <Col className='d-flex align-items-center'>{task.name}</Col>
+                                    <Col className='d-flex align-items-center'>{task.status}</Col>
+                                    <Col>
+                                        <Button onClick={() => this.deleteTask(task.id)} className='float-right' variant='danger'>
+                                            Delete
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Alert>
+                        ))}
                 </Container>
             </div>
         );
